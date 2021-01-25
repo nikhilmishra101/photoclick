ed1a3ecd719177eef02e52c0dce74d7db2d2a26d
IEIrodov <nikhilindia30111999@gmail.com>
2021-01-25 08:55:19 +0530

    AboutUsSection

---
+++ src/components/AboutSection.js
@@ -0,0 +1,32 @@
+import React from "react";
+import home1 from "../img/home1.png";
+
+const AboutSection = () => {
+  return (
+    <div className="description">
+      <div className="title">
+        <div className="hide">
+          <h2>We work to make</h2>
+        </div>
+        <div className="hide">
+          <h2>
+            your <span>dreams</span>come
+          </h2>
+        </div>
+        <div className="hide">
+          <h2>true.</h2>
+        </div>
+        <p>
+          Contact us for any website or app development ideas that you have.We
+          have professionals with amazing skills.
+        </p>
+        <button>Contact Us.</button>
+      </div>
+      <div className="image">
+        <img src={home1} alt="computer with webstack" />
+      </div>
+    </div>
+  );
+};
+
+export default AboutSection;
---
+++ src/pages/AboutUs.js
@@ -0,0 +1,13 @@
+import React from "react";
+// Page Components
+import AboutSection from "../components/AboutSection";
+
+const AboutUs = () => {
+  return (
+    <div className="root">
+      <AboutSection />
+    </div>
+  );
+};
+
+export default AboutUs;
