f8f4c7f6b7e15126eb02a5238fe26a29577e8b85
IEIrodov <nikhilindia30111999@gmail.com>
2021-01-28 19:17:53 +0530

    8.Adding Nav

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import React from "react";
 import GlobalStyle from "./components/GlobalStyle";
 //Import Pages
 import AboutUs from "./pages/AboutUs";
+import Nav from "./components/Nav";
 function App() {
   return (
     <div className="App">
+      <Nav />
       <GlobalStyle />
       <AboutUs />
     </div>
