1a8e37c21d38bb0cff18f793f6e86965a592304a
IEIrodov <nikhilindia30111999@gmail.com>
2021-01-29 13:44:05 +0530

    14.Adding About Animation

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -3,6 +3,7 @@ import home1 from "../img/home1.png";
 import { About, Description, Hide, Image } from "../styles.js";
 //Framer motion
 import { motion } from "framer-motion";
+import { titleAnim, fade, photoAnim } from "../animation";
 
 const AboutSection = () => {
   return (
@@ -10,25 +11,29 @@ const AboutSection = () => {
       <Description>
         <motion.div>
           <Hide>
-            <motion.h2>We work to make</motion.h2>
+            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>
+            <motion.h2 variants={titleAnim}>
               your <span>dreams</span> come
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>true.</motion.h2>
+            <motion.h2 variants={titleAnim}>true.</motion.h2>
           </Hide>
         </motion.div>
-        <p>
+        <motion.p variants={fade}>
           Contact us for any website or app development ideas that you have.We
           have professionals with amazing skills.
-        </p>
-        <button>Contact Us.</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact Us.</motion.button>
       </Description>
       <Image>
-        <img src={home1} alt="computer with webstack" />
+        <motion.img
+          variants={photoAnim}
+          src={home1}
+          alt="computer with webstack"
+        />
       </Image>
     </About>
   );
