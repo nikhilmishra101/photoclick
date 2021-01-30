86d00b173c6101269ef627e28b09b200c89ae858
IEIrodov <nikhilindia30111999@gmail.com>
2021-01-30 09:22:02 +0530

    18.Finalising Faq Section

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { About } from "../styles";
 import Toggle from "./Toggle";
+import { AnimateSharedLayout } from "framer-motion";
 
 const FaqSection = () => {
   return (
@@ -9,43 +10,45 @@ const FaqSection = () => {
       <h2>
         Any Questions ? <span>FAQ</span>
       </h2>
-      <Toggle>
-        <div className="question">
-          <h4>How Do I Start?</h4>
+      <AnimateSharedLayout>
+        <Toggle title="How Do I Start?">
           <div className="answer">
             <p>Lorem ipsum color sit amet.Lorem ipsum color sit amet.</p>
-            <p>Lorem ipsum color sit amet.Lorem ipsum color sit amet.</p>
+            <p>
+              Lorem ipsum color sit amet.Lorem ipsum color sit amet. Lorem ipsum
+              color sit amet.Lorem ipsum color sit amet
+            </p>
           </div>
-          <div className="faq-line"></div>
-        </div>
-      </Toggle>
-      <Toggle>
-        <div className="question">
-          <h4>Daily Schedule</h4>
+        </Toggle>
+        <Toggle title="Daily Schedule">
           <div className="answer">
             <p>Lorem ipsum color sit amet.Lorem ipsum color sit amet.</p>
+            <p>
+              Lorem ipsum color sit amet.Lorem ipsum color sit amet Lorem ipsum
+              color sit amet.Lorem ipsum color sit amet
+            </p>
           </div>
-          <div className="faq-line"></div>
-        </div>
-      </Toggle>
-      <Toggle>
-        <div className="question">
-          <h4>Different Payment Methods</h4>
+        </Toggle>
+
+        <Toggle title="Different Payment Methods">
           <div className="answer">
             <p>Lorem ipsum color sit amet.Lorem ipsum color sit amet.</p>
+            <p>
+              Lorem ipsum color sit amet.Lorem ipsum color sit amet.Lorem ipsum
+              color sit amet.Lorem ipsum color sit amet
+            </p>
           </div>
-          <div className="faq-line"></div>
-        </div>
-      </Toggle>
-      <Toggle>
-        <div className="question">
-          <h4>What Products do you offer.</h4>
+        </Toggle>
+        <Toggle title="What Products do you offer.">
           <div className="answer">
             <p>Lorem ipsum color sit amet.Lorem ipsum color sit amet.</p>
+            <p>
+              Lorem ipsum color sit amet.Lorem ipsum color sit amet. Lorem ipsum
+              color sit amet.Lorem ipsum color sit amet
+            </p>
           </div>
-          <div className="faq-line"></div>
-        </div>
-      </Toggle>
+        </Toggle>
+      </AnimateSharedLayout>
     </Faq>
   );
 };
