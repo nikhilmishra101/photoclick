7abaec165220f6c82d24d6743fc42902be8a3fa5
IEIrodov <nikhilindia30111999@gmail.com>
2021-01-30 10:52:18 +0530

    19.Scroll Animation

--- src/animation.js
+++ src/animation.js
@@ -72,3 +72,14 @@ export const sliderContainer = {
     transition: { staggerChildren: 0.1, ease: "easeOut" },
   },
 };
+
+export const scrollReveal = {
+  hidden: { opacity: 0, scale: 1.2, transition: { duration: 0.5 } },
+  show: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
